b2adcd008df4a0cddc536220b2a0d61c05e34b81
J-Barta <53406552+J-Barta@users.noreply.github.com>
2022-07-08 08:46:13 -0400

    add some robot manager code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,15 +1,26 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.util.RobotManager;
+
 public class RobotContainer {
+
+  private final RobotManager robotManager = new RobotManager();
 
   public RobotContainer() {
 
+    robotManager.enable();
 
     configureButtonBindings();
   }
 
 
   private void configureButtonBindings() {}
+
+  
+  public Command determineRobotManagerState() {
+    return robotManager.goToState(robotManager.getEntryState());
+  }
 
 
 }
