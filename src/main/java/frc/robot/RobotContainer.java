514bc7a1c22d0212ed16df555b03b5699fa6ad08
J-Barta <53406552+J-Barta@users.noreply.github.com>
2022-07-08 08:47:12 -0400

    add joysticks

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,15 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.util.RobotManager;
 
 public class RobotContainer {
+  
+  private final Joystick driverController = new Joystick(0);
+  private final Joystick operatorController = new Joystick(1);
 
+  
   private final RobotManager robotManager = new RobotManager();
 
   public RobotContainer() {
